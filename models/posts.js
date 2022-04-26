f68302fe9b6b9d87cb2923ff1d63866ace052d87
Jimmy ChangWenChi <geograp232@gmail.com>
2022-04-26 21:41:35 +0800

    新增檔案

---
+++ models/posts.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            require:[true,"姓名未填寫"]
+        },
+        tags:{
+            type:String,
+            require:[true,"tags 未填寫"]
+        },
+        content:{
+            type:String,
+            require:[true,"內容未填寫"]
+        },
+        createAt:{
+            type:Date,
+            default:Date.now,
+            select:false
+        },
+        likes:{
+            type:Number,
+            default:0
+        }
+    },
+    {
+        versionKey:false,
+        collection:"posts"
+    }
+)
+
+const POST = mongoose.model("posts",schema)
+
+module.exports = POST;
